688dd30f273162a6c97d0f33afd84251b7dcebfd
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-20 20:00:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,11 @@
 package com.company;
 
 public class Main {
+    
 
     public static void main(String[] args) {
-        EstacionamentoController estController = new EstacionamentoController(4);
+        EstacionamentoController estController = new EstacionamentoController();
         
     }
 
-
-    public void criaEstacionamento(int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
-        Estacionamento estacionamento = new Estacionamento( qtdPreferencial, qtdCarro, qtdMoto, qtdPreferencial);
-    }
-
-    public void criaMoto( String placa) {
-        this.veiculo = new Moto(placa);
-    }
-
-    public void criaCarro( String placa) {
-        this.veiculo = new Carro(placa);
-    }
-
-    public void criaCamionete( String placa) {
-        this.veiculo = new Camionete(placa);
-    }
 }
