99de4f2936a319e9a4e125a1d124af9b14dab7b5
cesar.mesquita <cesar.mesquita@ilegra.com>
2020-05-20 17:45:50 -0300

    Modificado: Criado Classe CabineControle/ Modifcado: EstacionamentoController, Main

--- CabineControle.java
+++ CabineControle.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class EstacionamentoController {
+public class CabineControle {
 
     public void efetuaEntrada(){ // valida e efetua a entrada de veiculos no estacionamento
     }
@@ -8,4 +8,9 @@ public class EstacionamentoController {
     public void efetuaSaida(){ // valida e efetua a saida de veiculos no estacionamento
     }
 
+    public void calculaCobranca(){ // calcula o valor a ser pago pelo motorista
+    }
+
+    public void consultaVagas(){ // consulta as placas dos veiculos presentes no estacionamento
+    }
 }
--- Main.java
+++ Main.java
@@ -6,4 +6,21 @@ public class Main {
         EstacionamentoController estController = new EstacionamentoController(4);
         
     }
+
+
+    public void criaEstacionamento(int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
+        Estacionamento estacionamento = new Estacionamento( qtdPreferencial, qtdCarro, qtdMoto, qtdPreferencial);
+    }
+
+    public void criaMoto( String placa) {
+        this.veiculo = new Moto(placa);
+    }
+
+    public void criaCarro( String placa) {
+        this.veiculo = new Carro(placa);
+    }
+
+    public void criaCamionete( String placa) {
+        this.veiculo = new Camionete(placa);
+    }
 }
