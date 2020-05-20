6197f6c73d32c9ca37b2e0a3a07b96c551ae6386
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-20 20:01:32 -0300

    Update EstacionamentoController.java

--- EstacionamentoController.java
+++ EstacionamentoController.java
@@ -1,11 +1,26 @@
 package com.company;
 
 public class EstacionamentoController {
+    Object veiculo;
 
-    public void efetuaEntrada(){ // valida e efetua a entrada de veiculos no estacionamento
+    public void criaCabine(double tarifaBase){
+        CabineControle cabine = new CabineControle(tarifaBase);
     }
 
-    public void efetuaSaida(){ // valida e efetua a saida de veiculos no estacionamento
+    public void criaEstacionamento(int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
+        Estacionamento estacionamento = new Estacionamento( qtdPreferencial, qtdCarro, qtdMoto, qtdPreferencial);
     }
 
+    public void criaMoto( String placa) {
+        this.veiculo = new Moto(placa);
+    }
+
+    public void criaCarro( String placa) {
+        this.veiculo = new Carro(placa);
+    }
+
+    public void criaCamionete( String placa) {
+        this.veiculo = new Camionete(placa);
+    }
+    
 }
