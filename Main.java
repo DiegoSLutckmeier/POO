de8abecea09ba5d07a2a1283d9fe2b0414745eec
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-25 21:40:14 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,30 +4,59 @@ import java.util.Scanner;
 
 public class Main {
 
+    private double tarifaBase;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         EstacionamentoController estController = new EstacionamentoController();
-        int opcaoEscolhida = 0;
 
+        int opcaoEscolhida = 0;
+        int opcaoTipoVeiculo = 0;
         boolean retorno = false;
 
         while (!retorno) {
             System.out.println("Escolha uma operação do estacionamento: \n");
             System.out.println("1 - Criar cabine passando a tarifa básica.");
             System.out.println("2 - Criar estacionamento passando a capacidade de vagas(qtd Preferencial, qtd Carros, qtd Motos, qtd Camionetes)");
-            System.out.println("3 - Criar veiculo para inserir.");
-            System.out.println("4 - Retirar veiculo do estacionamento.");
+            System.out.println("3 - Entrada de veículo.");
+            System.out.println("4 - Saída de veículo.");
             System.out.println("5 - Listar todas os veiculos que ocupam o estacionamento.");
             System.out.println("0 - Exit.");
             opcaoEscolhida = scanner.nextInt();
 
             switch (opcaoEscolhida) {
                 case 1:
+                    estController.criaCabine(10);
+                    System.out.println(estController.cabine.tarifaBase); // teste
                     continue;
                 case 2:
+                    estController.criaEstacionamento(10,10,10,10);
+                    System.out.println(estController.estacionamento.qtdCarro); //teste
                     continue;
-                case 3: // extende opcao de selecionar o tipo de veiculo
+                case 3: // extende a opcao de selecionar o tipo de veiculo
+                    System.out.println("Escolha um tipo de veiculo: \n");
+                    System.out.println("1 - Carro.");
+                    System.out.println("2 - Moto.");
+                    System.out.println("3 - Camionete.");
+                    System.out.println("0 - Voltar.");
+                    opcaoTipoVeiculo = scanner.nextInt();
+
+                    switch(opcaoTipoVeiculo) {
+                        case 1:
+                            estController.criaCarro("AGS5F26");
+                            System.out.println(estController.veiculo);
+                            continue;
+                        case 2:
+                            estController.criaMoto("ALS4O78");
+                            System.out.println(estController.veiculo);
+                            continue;
+                        case 3:
+                            estController.criaCamionete("ALD8W15");
+                            System.out.println(estController.veiculo);
+                            continue;
+                        case 0:
+                            continue;
+                    };
                     continue;
                 case 4:
                     continue;
@@ -37,7 +66,7 @@ public class Main {
                     retorno = true;
             }
         }
-        
+
     }
 
 }
