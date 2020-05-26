e966cc14398c4dd70fadc54ba28f32fe1aabb115
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 01:41:42 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -4,24 +4,55 @@ import java.util.ArrayList;
 
 public class Estacionamento {
     ArrayList placasPresentes = new ArrayList();
-    int qtdPreferencial;
-    int qtdCarro;
-    int qtdMoto;
-    int qtdCamionete;
+    ArrayList ocupacaoEstacionamento = new ArrayList();
+//    int qtdPreferencial;
+//    int qtdCarro;
+//    int qtdMoto;
+//    int qtdCamionete;
 
     public Estacionamento( int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
-        VagaPreferencial vagasPreferenciais[] = new VagaPreferencial[qtdPreferencial];
-        VagaCarro vagasCarros[] = new VagaCarro[qtdCarro];
-        VagaMoto vagasMotos[] = new VagaMoto[qtdMoto];
-        VagaCamionete vagasCamionetes[] = new VagaCamionete[qtdCamionete];
+        for (int i = 0; i < qtdPreferencial; i++) {
+            VagaPreferencial vaga = new VagaPreferencial();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdCarro; i++) {
+            VagaCarro vaga = new VagaCarro();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdCamionete; i++) {
+            VagaCamionete vaga = new VagaCamionete();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdMoto; i++) {
+            VagaMoto vaga = new VagaMoto();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        System.out.println(ocupacaoEstacionamento.size()); // teste
     }
 
     public double calcularOcupacao() { // retorna o percentual de ocupacao do estacionamento
         return 100;
     }
 
-    public boolean ocupaVaga() { // insere o veiculo na posicao correta
-        return true;
+    public void ocupaVaga( String tipoVaga) { // insere o veiculo na posicao correta
+
+        for (int i = 0; i < this.ocupacaoEstacionamento.size(); i++) {
+            Object obj = this.ocupacaoEstacionamento.get(i);
+            Class temp = obj.getClass();
+
+            if ( temp.getName() == tipoVaga) {
+                System.out.println(i);
+                System.out.println(obj);
+
+
+                // não consigo retornar o method de VagaMoto = obj
+            }
+        }
+
     }
 
     public boolean desocupaVaga() { // remove o veiculo da posicao correta
