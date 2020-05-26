a8b1e4d641572ac53af052375dd1757b1f55e834
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 01:40:43 -0300

    Update CabineControle.java

--- CabineControle.java
+++ CabineControle.java
@@ -1,13 +1,19 @@
 package com.company;
 
 public class CabineControle {
-    double tarifaBase;
+    protected double tarifaBase;
+    Estacionamento estacionamento;
 
     public CabineControle(double tarifaBase){
         this.tarifaBase = tarifaBase;
     }
 
+    public void criaEstacionamento(int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
+        this.estacionamento = new Estacionamento( qtdPreferencial, qtdCarro, qtdMoto, qtdPreferencial);
+    }
+
     public void efetuaEntrada(){ // valida e efetua a entrada de veiculos no estacionamento
+        this.estacionamento.ocupaVaga("com.company.VagaMoto");
     }
 
     public void efetuaSaida(){ // valida e efetua a saida de veiculos no estacionamento
