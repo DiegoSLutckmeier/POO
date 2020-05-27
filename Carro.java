7bda2b380d402fbb0611c6d9aef06554a091ca62
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 22:42:59 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,13 +1,7 @@
 package com.company;
 
 public class Carro extends Veiculo{
-    boolean preferencial;
-
     public Carro( String placa) {
         this.placa = placa;
-    }
-    
-    public void setPreferencial(boolean preferencial){
-        this.preferencial = preferencial;
     }
 }
