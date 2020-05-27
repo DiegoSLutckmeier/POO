fef893d6a9dc62f95cf765fdbad86a91aa586531
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 22:43:40 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -3,25 +3,60 @@ package com.company;
 import java.util.ArrayList;
 
 public class Estacionamento {
-    ArrayList placasPresentes = new ArrayList();
-    int qtdPreferencial;
-    int qtdCarro;
-    int qtdMoto;
-    int qtdCamionete;
+    ArrayList<Vaga> ocupacaoEstacionamento = new ArrayList<Vaga>();
 
     public Estacionamento( int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
-        VagaPreferencial vagasPreferenciais[] = new VagaPreferencial[qtdPreferencial];
-        VagaCarro vagasCarros[] = new VagaCarro[qtdCarro];
-        VagaMoto vagasMotos[] = new VagaMoto[qtdMoto];
-        VagaCamionete vagasCamionetes[] = new VagaCamionete[qtdCamionete];
+        for (int i = 0; i < qtdPreferencial; i++) {
+            VagaPreferencial vaga = new VagaPreferencial();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdCarro; i++) {
+            VagaCarro vaga = new VagaCarro();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdCamionete; i++) {
+            VagaCamionete vaga = new VagaCamionete();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        for (int i = 0; i < qtdMoto; i++) {
+            VagaMoto vaga = new VagaMoto();
+            this.ocupacaoEstacionamento.add(vaga);
+        }
+
+        System.out.println("Quantidade total de vagas: "+ocupacaoEstacionamento.size()+"\n"); // teste
     }
 
     public double calcularOcupacao() { // retorna o percentual de ocupacao do estacionamento
         return 100;
     }
 
-    public boolean ocupaVaga() { // insere o veiculo na posicao correta
-        return true;
+    public int verificaDisponibilidade( String tipoVaga) { // insere o veiculo na posicao correta
+        int posicaoEstacionamento;
+        posicaoEstacionamento = -1;
+        
+        for (int i = 0; i < this.ocupacaoEstacionamento.size(); i++) {
+            Vaga obj = this.ocupacaoEstacionamento.get(i);
+            Class temp = obj.getClass();
+
+            if ( temp.getName().equals(tipoVaga)) {
+
+                if ( !obj.statusOcupacao()) {
+                    posicaoEstacionamento = i;
+                    break;
+                }
+            }
+        }
+
+        return posicaoEstacionamento;
+    }
+
+    public void alocaVaga( int posicaoEstacionamento, Veiculo veiculo) {
+        Vaga obj = this.ocupacaoEstacionamento.get(posicaoEstacionamento);
+
+        obj.ocupaVaga( veiculo);
     }
 
     public boolean desocupaVaga() { // remove o veiculo da posicao correta
