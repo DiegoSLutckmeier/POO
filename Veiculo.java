14300c0ba379c5e857277ce6d47344be21449437
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 22:48:41 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -4,6 +4,7 @@ public class Veiculo {
     protected String placa;
     protected String modelo;
     protected String fabricante;
+    boolean preferencial = false;
 
     public void setModelo(String modelo){
         this.modelo = modelo;
