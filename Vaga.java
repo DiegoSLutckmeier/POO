829e90c7686d5c4c14c36f84ba5aa311f707613d
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-05-26 22:46:03 -0300

    Update Vaga.java

--- Vaga.java
+++ Vaga.java
@@ -3,6 +3,22 @@ package com.company;
 import java.util.Date;
 
 public class Vaga {
-    Object ocupaVaga;
-    Date tempoEntrada = new Date();
+    Veiculo veiculoVaga;
+    Date tempoEntrada;
+
+    public void ocupaVaga( Veiculo veiculo) {
+        this.veiculoVaga = veiculo;
+        this.tempoEntrada = new Date();
+    }
+
+    public boolean statusOcupacao() {
+        boolean retorno;
+        if (this.veiculoVaga == null) {
+            retorno = false;
+        } else {
+            retorno = true;
+        }
+
+        return retorno;
+    }
 }
