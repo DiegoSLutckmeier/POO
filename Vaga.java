252863f40f64e8b8845486702f402da87bd44dcb
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-06-03 17:37:31 -0300

    Update Vaga.java

--- Vaga.java
+++ Vaga.java
@@ -1,14 +1,17 @@
 package com.company;
 
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Vaga {
     Veiculo veiculoVaga;
-    Date tempoEntrada;
+    Ticket ticketAtual;
+    ArrayList<Ticket> vagaTickets = new ArrayList<Ticket>();
 
     public void ocupaVaga( Veiculo veiculo) {
         this.veiculoVaga = veiculo;
-        this.tempoEntrada = new Date();
+        this.ticketAtual = new Ticket(veiculo.getPlaca());
+        vagaTickets.add(ticketAtual);
     }
 
     public boolean statusOcupacao() {
@@ -21,4 +24,31 @@ public class Vaga {
 
         return retorno;
     }
+
+    public Ticket getTicketAtual() {
+        return this.ticketAtual;
+    }
+
+    public Veiculo getVeiculoVaga() {
+        return this.veiculoVaga;
+    }
+
+    public void getVagaTickets() {
+        if (this.vagaTickets.size()>0) {
+            for (int i = 0; i < this.vagaTickets.size(); i++) {
+                Ticket obj = this.vagaTickets.get(i);
+
+                System.out.println(obj.getTicketID());
+            }
+        } else {
+            System.out.println("Essa vaga não possui tickets.");
+        }
+
+    }
+
+    public void resetaVaga() {
+        this.veiculoVaga = null;
+        this.ticketAtual = null;
+    }
+
 }
