6ce569d70b2944dd1d9d15baae1729993526613e
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-06-03 17:36:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,36 +12,26 @@ public class Main {
 
         int opcaoEscolhida = 0;
         int opcaoTipoVeiculo = 0;
+        int opcaoOperacaoTickets = 0;
         boolean retorno = false;
 
         while (!retorno) {
             System.out.println("Escolha uma operação do estacionamento: \n");
-            System.out.println("1 - Criar cabine passando a tarifa básica.");
-            System.out.println("2 - Criar estacionamento passando a capacidade de vagas(qtd Preferencial, qtd Carros, qtd Motos, qtd Camionetes)");
-            System.out.println("3 - Entrada de veículo.");
-            System.out.println("4 - Saída de veículo.");
-            System.out.println("5 - Listar todas os veiculos que ocupam o estacionamento.");
+            System.out.println("1 - Criar cabine passando a tarifa básica. ");
+            System.out.println("2 - Criar estacionamento passando a capacidade de vagas. ");
+            System.out.println("3 - Entrada de veículo. ");
+            System.out.println("4 - Saída de veículo. ");
+            System.out.println("5 - Listar todos os veiculos que ocupam o estacionamento. ");
+            System.out.println("6 - Operações de Tickets. ");
             System.out.println("0 - Exit. \n");
             opcaoEscolhida = scanner.nextInt();
 
             switch (opcaoEscolhida) {
                 case 1:
-                    System.out.println("Passe a tarifa base: ");
-                    double tarifaBase = scanner.nextDouble();
-                    estController.criaCabine(tarifaBase);
-                    System.out.println("Tarifa base: "+estController.cabine.tarifaBase+"\n"); // teste
+                    estController.criaCabine();
                     continue;
                 case 2:
-                    System.out.println("Passe as quantidades referentes na sequência abaixo: ");
-                    System.out.println("Quantidade de vagas preferenciais: ");
-                    int qtdPreferencial = scanner.nextInt();
-                    System.out.println("Quantidade de vagas de carros: ");
-                    int qtdCarro = scanner.nextInt();
-                    System.out.println("Quantidade de vagas de moto: ");
-                    int qtdMoto = scanner.nextInt();
-                    System.out.println("Quantidade de vagas de camionetes: ");
-                    int qtdCamionete = scanner.nextInt();
-                    estController.cabine.criaEstacionamento(qtdPreferencial,qtdCarro,qtdMoto,qtdCamionete);
+                    estController.cabine.criaEstacionamento();
                     continue;
                 case 3: // extende a opcao de selecionar o tipo de veiculo
                     boolean retornoInsercao = false;
@@ -56,15 +46,15 @@ public class Main {
 
                         switch (opcaoTipoVeiculo) {
                             case 1:
-                                estController.criaCarro("AGS5F26");
+                                estController.criaCarro();
                                 estController.cabine.efetuaEntrada(estController.veiculo);
                                 continue;
                             case 2:
-                                estController.criaMoto("ALS4O78");
+                                estController.criaMoto();
                                 estController.cabine.efetuaEntrada(estController.veiculo);
                                 continue;
                             case 3:
-                                estController.criaCamionete("ALD8W15");
+                                estController.criaCamionete();
                                 estController.cabine.efetuaEntrada(estController.veiculo);
                                 continue;
                             case 0:
@@ -74,10 +64,34 @@ public class Main {
 
                     continue;
                 case 4:
+                    int posicao;
+                    System.out.println("Qual posição gostaria de retirar? ");
+                    posicao = scanner.nextInt();
+                    estController.cabine.efetuaSaida(posicao);
                     continue;
                 case 5:
+                    estController.cabine.estacionamento.getOcupacaoDasVagas();
                     continue;
-                case 9:
+                case 6:
+                    boolean retornoTickets = false;
+
+                    while (!retornoTickets) {
+                        System.out.println("\nEscolha uma operação: \n");
+                        System.out.println("1 - Retornar histórico de tickets de uma vaga.");
+                        System.out.println("0 - Voltar. \n");
+                        opcaoOperacaoTickets = scanner.nextInt();
+
+                        switch (opcaoOperacaoTickets) {
+                            case 1:
+                                int posicaoEstacionamento = 0;
+                                System.out.println("Escolha uma posição para inspecionar: ");
+                                posicaoEstacionamento = scanner.nextInt();
+                                estController.cabine.estacionamento.getVaga(posicaoEstacionamento).getVagaTickets();
+                                continue;
+                            case 0:
+                                retornoTickets = true;
+                        }
+                    }
                     continue;
                 case 0:
                     retorno = true;
