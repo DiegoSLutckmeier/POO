e724f925f1295055ca767460d705613a24f3e618
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-06-03 17:35:59 -0300

    Update EstacionamentoController.java

--- EstacionamentoController.java
+++ EstacionamentoController.java
@@ -1,23 +1,53 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class EstacionamentoController {
+    Scanner scanner = new Scanner(System.in);
     Veiculo veiculo;
     CabineControle cabine;
 
-    public void criaCabine(double tarifaBase){
-        this.cabine = new CabineControle(tarifaBase);
+    public void criaCabine(){
+        this.cabine = CabineControle.getInstance();
     }
 
-    public void criaMoto( String placa) {
+    public void criaMoto( ) {
+        System.out.println("Qual a placa do veículo? ");
+        String placa = scanner.nextLine();
+
         this.veiculo = new Moto(placa);
     }
 
-    public void criaCarro( String placa) {
+    public void criaCarro( ) {
+        System.out.println("Qual a placa do veículo? ");
+        String placa = scanner.nextLine();
+
         this.veiculo = new Carro(placa);
+        System.out.println("Este carro é preferencial? ");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+        int retorno = scanner.nextInt();
+        scanner.nextLine();
+
+        if (retorno==1) {
+            this.veiculo.setPreferencial();
+        }
     }
 
-    public void criaCamionete( String placa) {
+    public void criaCamionete( ) {
+        System.out.println("Qual a placa do veículo? ");
+        String placa = scanner.nextLine();
+
         this.veiculo = new Camionete(placa);
+        System.out.println("Esta camionete é preferencial? ");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+        int retorno = scanner.nextInt();
+        scanner.nextLine();
+
+        if (retorno==1) {
+            this.veiculo.setPreferencial();
+        }
     }
 
 }
