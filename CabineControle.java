646a7c0c86ecbcb523065d1a385106ed3da4ea7c
DiegoSLutckmeier <42702836+DiegoSLutckmeier@users.noreply.github.com>
2020-06-03 17:34:51 -0300

    Update CabineControle.java

--- CabineControle.java
+++ CabineControle.java
@@ -1,6 +1,11 @@
 package com.company;
 
+import java.util.Observer;
+import java.util.Scanner;
+
 public class CabineControle {
+
+    private static CabineControle INSTANCE;
     protected double tarifaBase;
     Estacionamento estacionamento;
 
@@ -8,15 +13,22 @@ public class CabineControle {
         this.tarifaBase = tarifaBase;
     }
 
-    public void criaEstacionamento(int qtdPreferencial, int qtdCarro, int qtdMoto, int qtdCamionete){
-        this.estacionamento = new Estacionamento( qtdPreferencial, qtdCarro, qtdMoto, qtdPreferencial);
+    public void criaEstacionamento(){
+        this.estacionamento = Estacionamento.getInstance();
     }
 
     public void efetuaEntrada( Veiculo veiculo){ // valida e efetua a entrada de veiculos no estacionamento
         int posicaoEstacionamento;
         Object tipoVeiculo = veiculo;
+        String tipo;
 
-        posicaoEstacionamento = this.estacionamento.verificaDisponibilidade( "com.company.Vaga"+tipoVeiculo.getClass().getSimpleName()); //"com.company.VagaMoto
+        if (veiculo.preferencial==true) {
+            tipo = "Preferencial";
+        } else {
+            tipo = tipoVeiculo.getClass().getSimpleName();
+        }
+
+        posicaoEstacionamento = this.estacionamento.verificaDisponibilidade( "com.company.Vaga"+tipo); //"com.company.VagaMoto
         System.out.println("Posição onde se pode estacionar: "+posicaoEstacionamento);
 
         if ( posicaoEstacionamento == -1) {
@@ -27,12 +39,49 @@ public class CabineControle {
         }
     }
 
-    public void efetuaSaida(){ // valida e efetua a saida de veiculos no estacionamento
+    public void efetuaSaida( int posicaoVeiculo){ // valida e efetua a saida de veiculos no estacionamento
+        if (posicaoVeiculo < this.estacionamento.ocupacaoEstacionamento.size()) {
+            Vaga vaga = this.estacionamento.getVaga(posicaoVeiculo);
+            if (vaga.statusOcupacao()) {
+                this.calculaCobranca(vaga);
+            } else {
+                System.out.println("Não existe veículo nesta vaga.");
+            }
+        } else {
+            System.out.println("Essa vaga não existe no estacionamento.");
+        }
+
     }
 
-    public void calculaCobranca(){ // calcula o valor a ser pago pelo motorista
+    public void calculaCobranca( Vaga vaga){ // calcula o valor a ser pago pelo motorista
+        Ticket tkt = vaga.getTicketAtual();
+
+        //efetua a cobrança
+        tkt.setTempoSaida();
+        System.out.println("Tarifa base: "+this.tarifaBase);
+        System.out.println("Ticket do veículo: "+tkt.getTicketID().toString());
+        System.out.println("Entrada no estacionamento: "+tkt.getTempoEntrada().toString());
+        System.out.println("Saída do estacionamento: "+tkt.getTempoSaida().toString());
+
+        //reseta as infos da vaga
+        vaga.resetaVaga();
     }
 
     public void consultaVagas(){ // consulta as placas dos veiculos presentes no estacionamento
+    }
+
+    public static CabineControle getInstance( ) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (INSTANCE == null) {
+            System.out.println("Passe a tarifa base: ");
+            double tarifaBase = scanner.nextDouble();
+            INSTANCE = new CabineControle(tarifaBase);
+            System.out.println("Tarifa base: "+tarifaBase+"\n");
+        } else {
+            System.out.println("Classe já criada!");
+        }
+
+        return INSTANCE;
     }
 }
